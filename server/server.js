2505f683f6a6087b5333ebdeb86c9316357f9e16
MbusyaNgesa <theengesa@gmail.com>
2025-08-07 23:52:42 +0300

    Message Routes

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./src/routes/v1/authRoute.js";
+import messageRoutes from "./src/routes/v1/messageRoute.js";
 import { connectDB } from "./src/lib/connectDB.js";
 
 dotenv.config();
@@ -12,7 +13,9 @@ const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running");
