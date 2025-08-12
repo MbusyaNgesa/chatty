8aafb0d3a2667154fe1ef884f796ca3d07fcb4a5
MbusyaNgesa <theengesa@gmail.com>
2025-08-12 22:22:38 +0300

    Zustand Initialization

---
+++ client/src/pages/SignUpPage.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { useAuthStore } from "../store/useAuthStore";
+
+const SignUpPage = () => {
+  // const {showPassword, setShowPassword} = useState(false)
+  // const {formData, setFormData} = useState({
+  //   fullName: "",
+  //   email: "",
+  //   password: "",
+  // })
+
+  // const {signup, isSignUp} = useAuthStore();
+
+  // const validateForm = () => {
+
+  // }
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault()
+  // }
+
+  return <div>SignUpPage</div>;
+};
+
+export default SignUpPage;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./src/routes/v1/authRoute.js";
 import messageRoutes from "./src/routes/v1/messageRoute.js";
@@ -13,6 +14,12 @@ const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
